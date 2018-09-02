23e5a3a5a63277375e327765f9b37520082704ca
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-09-01 22:07:32 -0300

    Models (Usuário e Endereço)

---
+++ CadeMeuPet/CadeMeuPet/Models/Endereco.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuPet.Models
+{
+    [Table("Endereco")]
+    public class Endereco
+    {
+        [Key]
+        public int EnderecoId { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(150, ErrorMessage = "O endereço deve ter no máximo 150 caracteres!")]
+        [Display(Name = "Endereço")]
+        public string Logradouro { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(6, ErrorMessage = "O número deve ter no máximo 6 caracteres!")]
+        [Display(Name = "Numero")]
+        public string Numero { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(8, ErrorMessage = "O CEP deve ter no máximo 8 caracteres!")]
+        [Display(Name = "CEP")]
+        public string CEP { get; set; }
+
+        [Display(Name = "Latitude")]
+        [Column(TypeName = "VARCHAR")]
+        public string Latitude { get; set; }
+
+        [Display(Name = "Longitude")]
+        [Column(TypeName = "VARCHAR")]
+        public string Longitude { get; set; }
+
+    }
+}
---
+++ CadeMeuPet/CadeMeuPet/Models/Usuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuPet.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(45, ErrorMessage = "O nome deve ter no máximo 45 caracteres")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "o campo Login é obrigatório")]
+        [MaxLength(20, ErrorMessage = "O Login deve ter no máximo 20 caracteres")]
+        [Display(Name = "Login")]
+        public string  UserName { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "o campo Senha é obrigatório")]
+        [MaxLength(60, ErrorMessage = "A senha deve ter no máximo 60 caracteres")]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(12, ErrorMessage = "O telefone deve ter no máximo 12 caracteres")]
+        [Display(Name = "Login")]
+        public string Telefone { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Required(ErrorMessage = "o campo Email é obrigatório")]
+        [MaxLength(100, ErrorMessage = "A senha deve ter no máximo 100 caracteres")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "o campo Permissão é obrigatório")]
+        [Display(Name = "Permissão de usuário")]
+        public bool IsAdmin { get; set; }
+
+        [Display(Name = "Endereço")]
+        public int EnderecoId { get; set; }
+
+        public virtual Endereco Endereco { get; set; }
+
+
+
+
+    }
+}
