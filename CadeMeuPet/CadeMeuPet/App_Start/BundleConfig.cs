61b190a10f2c5db9c69ee2fcb811980fdf303361
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-09-02 00:24:41 -0300

    Algumas alterações na view do layout e add Jquery

---
+++ CadeMeuPet/CadeMeuPet/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace CadeMeuPet
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new Bundle("~/js-global").Include(
+                "~/scripts/jquery-3.3.1.min.js",
+                "~/scripts/bootstrap.min.js"
+            ));
+
+        }
+    }
+}
